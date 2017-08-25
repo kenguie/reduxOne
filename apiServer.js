fc35c1194f70699f0d52a9ee83c9c29593f76ce0
Kenrick Guie <nyhunter77@yahoo.com>
2017-08-24 21:53:41 -0400

    moved mongo db to mlab

--- apiServer.js
+++ apiServer.js
@@ -17,7 +17,12 @@ app.use(cookieParser());
 
 // APIs
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/bookshop');
+
+// MONGO LAB
+mongoose.connect('mongodb://testUser:test@ds157873.mlab.com:57873/bookshop');
+
+// LOCAL DB
+// mongoose.connect('mongodb://localhost:27017/bookshop');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, '# MongoDB - connection error: '));
